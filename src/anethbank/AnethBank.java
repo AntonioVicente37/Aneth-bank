70ba54be265793f796763ff2b019905a961fd921
AntonioVicente37 <githantonio@gmail.com>
2022-02-06 20:11:01 +0100

    Adicionando o menu das operacoes do banco

--- src/anethbank/AnethBank.java
+++ src/anethbank/AnethBank.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package anethbank;
+import java.util.Scanner;
 import modelpackage.bank.Cliente;
 
 /**
@@ -21,8 +22,56 @@ public class AnethBank {
         System.out.println("                               ANETH BANKL                                                            ");
         System.out.println("-------------------------------------------------------------------------------");
         
-        Cliente c1 = new Cliente();
-        c1.getNome();
+        Scanner input = new Scanner(System.in);
+        
+        int op = 0;        
+        int oc = 0;
+        // Menu de opcoes do sistema bancario
+       do{
+        System.out.println("1. Cliente");
+        System.out.println("2. Criar conta corrente");
+        System.out.println("3. Cria conta poupanca");
+        System.out.println("4. Conta");
+        System.out.println("5. Sair");
+        op = input.nextInt();
+        
+        if(op == 1){
+            System.out.println("\n1.Cadastrar cliente\n2.Listar Clientes");
+            oc = input.nextInt();
+            if(oc == 1){
+                  //System.out.println("Cliente cadastrado com sucesso|");
+            }else if(oc == 2){
+                
+               /// System.out.println("Conta Criada com sucesso|");
+            }else{
+                System.out.println("Opcao nao existente|");
+            }          
+        }else if(op == 2){
+            System.out.println("Conta corrente criada com sucesso");
+        }else if(op == 3){
+             System.out.println("Conta poupanca criada com sucesso");
+        }else if(op == 4){
+            System.out.println("Qual e o tipo de opercao que deseja executar?"
+                    + "\n1.saldo da conta\n2.extrato da conta\n3.deposito\n4.levantamento\n5.Tranferencia");
+            oc = input.nextInt();
+            if(oc == 1){
+                System.out.println("Consultar saldoe: ");             
+            }else if(oc == 2){
+                System.out.println("O seu extrato bancario: ");             
+            }else if(oc == 3){
+                System.out.println("deposito realizado com suceso: ");             
+            }else if(oc == 4){
+                System.out.println("levantamento realizado com sucesso: ");             
+            }else if(oc == 5){
+                System.out.println("Transferencia realizada com sucesso: ");             
+            }else{
+                System.out.println("Operacao invalida");
+            }
+        }else if(op > 5){
+            System.out.println("Opcao nao exite no nosso sistema");
+        }
+        
+       }while(op !=5);
     }
     
 }
