c0450a153018c52e0f50ec444129d4860e2ff47b
AntonioVicente37 <githantonio@gmail.com>
2022-02-06 09:15:33 +0100

    Iniciando o desenvolvimento da main e do menu dde apresentacao do nosso projeto

---
+++ src/anethbank/AnethBank.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package anethbank;
+import modelpackage.bank.Cliente;
+
+/**
+ *
+ * @author Engº António Vicente
+ */
+public class AnethBank {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("-------------------------------------------------------------------------------");
+        System.out.println("                               ANETH BANKL                                                            ");
+        System.out.println("-------------------------------------------------------------------------------");
+        
+        Cliente c1 = new Cliente();
+        c1.getNome();
+    }
+    
+}
---
+++ src/modelpackage/bank/Cliente.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelpackage.bank;
+
+/**
+ *
+ * @author Engº António Vicente
+ */
+public class Cliente {
+    // iniciando a criacao da classe clinete 
+    private String nome;
+    private int nif;
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getNif() {
+        return nif;
+    }
+
+    public void setNif(int nif) {
+        this.nif = nif;
+    }
+    
+    
+}
