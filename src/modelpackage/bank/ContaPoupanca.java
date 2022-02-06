2c518a983ea9e2875e963602b23fe005485faddc
AntonioVicente37 <githantonio@gmail.com>
2022-02-06 10:52:26 +0100

    Adicionando a classe conta poupanca no projeto

---
+++ src/modelpackage/bank/ContaPoupanca.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelpackage.bank;
+
+/**
+ *
+ * @author Engº António Vicente
+ */
+public class ContaPoupanca {
+    //Classe responsavel pela a criacao da conta poupanca
+}
