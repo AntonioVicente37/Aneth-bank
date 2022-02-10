20e45ec7a4c29cc07a561263b2d9ad532db3d978
AntonioVicente37 <githantonio@gmail.com>
2022-02-10 19:22:54 +0100

    Terminando a listagem e cadastro de clientes com arraylist

--- src/anethbank/AnethBank.java
+++ src/anethbank/AnethBank.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package anethbank;
+import java.util.ArrayList;
 import java.util.Scanner;
 import modelpackage.bank.Cliente;
 
@@ -19,7 +20,7 @@ public class AnethBank {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("-------------------------------------------------------------------------------");
-        System.out.println("                               ANETH BANKL                                     ");
+        System.out.println("                               ANETH BANK                                     ");
         System.out.println("-------------------------------------------------------------------------------");
         
         Scanner input = new Scanner(System.in);
@@ -39,13 +40,13 @@ public class AnethBank {
             System.out.println("\n1.Cadastrar cliente\n2.Listar Clientes");
             oc = input.nextInt();
             if(oc == 1){
+                // chamando a funcao responsavel pelo cadastramento de usuario
+                clienteCad();
                   //System.out.println("Cliente cadastrado com sucesso|");
             }else if(oc == 2){
                 System.out.println("Lista dos Clientes:");
-                Cliente c1 = new Cliente("Tino", 123);
-                System.out.println(c1);
-                Cliente c2 = new Cliente("Jose", 321);
-                System.out.println(c2);
+                //chamando a funcao responsaver pela listagem de clientes
+                clienteList();
                /// System.out.println("Conta Criada com sucesso|");
             }else{
                 System.out.println("Opcao nao existente|");
@@ -78,4 +79,38 @@ public class AnethBank {
        }while(op !=5);
     }
     
+   //funcao para o cadastro de clientes 
+  public static void clienteCad(){
+      Scanner input = new Scanner(System.in);
+        //Usando a Collection Arraylist para armazenar os dados dos clientes
+        ArrayList<Cliente> ls = new ArrayList<Cliente>();
+        // Variavel responsavel por controlar o laco do while 
+       char ct = 's';
+       do{
+           System.out.println("Digite o nome do cliente: ");
+           String n = input.next();
+           System.out.println("Digite o NIF do cliente");
+           int nf = input.nextInt();
+           Cliente c = new Cliente(n,nf);
+           //adicionando os dados dos clientes na lista
+           ls.add(c);
+           System.out.println("Deseja cadastrar um outro cliente? s ou S para sim.");
+           ct = input.next().charAt(0);
+       }while (ct == 's' || ct == 'S');
+       
+        for (Cliente i: ls){
+          System.out.println(i);
+                  
+      }
+  }
+  //Funcao responsavel por listar os clientes
+  public static void clienteList(){
+      /*for (cliente i: lista){
+          System.out.println(i);
+      }
+        /*Cliente c1 = new Cliente("Tino", 123);
+        System.out.println(c1);
+        Cliente c2 = new Cliente("Jose", 321);
+        System.out.println(c2);*/
+  }
 }
